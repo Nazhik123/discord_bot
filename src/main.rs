440b3ccdffcd0e79f6124ecfe66acbc178c61a9c
Nazhik123 <novozhilov_dp@mail.ru>
2023-08-03 16:23:21 +0300

    a1

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,62 @@
 extern crate serenity;
 
-use serenity::prelude::*;
-use serenity::model::gateway::Ready;
-use serenity::model::channel::Message;
-
+use std::io::Read;
 use std::fs::File;
-use std::io::prelude::*;
+use serenity::async_trait;
+use serenity::model::prelude::Reaction;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
 
 
 struct Handler;
 
-impl EventHandler for Handler{
-    fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "pong") {
-                println!("Error giving message: {:?}", why);
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+
+    async fn reaction_add(&self, ctx: Context, reaction: Reaction) {
+        let message_id: &str = "1134469951848202402";
+        let role_id1: u64 = 1134450517544808519; 
+        let role_id2: u64 = 1134451131288932373; 
+
+        if reaction.message_id.to_string() == message_id{
+            let role_id = match reaction.emoji.to_string().as_str() {
+                "😩" => role_id1,
+                "😁" => role_id2,
+                _ => {
+                    println!("Неизвестная реакция");
+                    return;
+                }
+            };
+            if let Some(user_id) = reaction.user_id {
+                if let Some(guild_id) = reaction.guild_id {
+                    if let Ok(mut member) = guild_id.member(&ctx, user_id).await {
+                        if let Err(why) = member.add_role(&ctx, role_id).await {
+                            println!("Ошибка при выдаче роли: {:?}", why);
+                        } else {
+                            println!("Роль успешно выдана");
+                        }
+                    }
+                }
             }
         }
     }
-
-    fn ready(&self, _: Context, ready:Ready) {
-        println!("{} is ready", ready.user.name);
-    }
-
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut file = File::open(".token").unwrap();
     let mut token: String = String::new();
     file.read_to_string(&mut token).expect("Error");
 
-    let mut client= Client::new(&token, Handler)
-                                            .expect("Error creating client");
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
 
-    if let Err(msg) = client.start() {
-        println!("Error: {:?}", msg);
+    if let Err(why) = client.start().await {
+        eprintln!("Client error: {:?}", why);
     }
-
 }
